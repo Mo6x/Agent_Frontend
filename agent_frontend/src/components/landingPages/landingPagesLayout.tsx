545f1cc3b74abb8aa487fd51c5b28a38966398d9
mo6x <christmoses1999@gmail.com>
2025-02-16 15:52:53 +0100

    working on it

---
+++ agent_frontend/src/components/landingPages/landingPagesLayout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { LandingPages } from "./landingPages"
+
+
+const LandingPagesLayout: React.FC = () => {
+  return (
+    <main>
+      <LandingPages />
+    </main>
+  );
+};
+
+export default LandingPagesLayout;
